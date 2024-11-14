cadd12b1b0f2026935811aa39c56f9599762e7a7
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-11-15 02:22:48 +0530

    MongoDB connection

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import connectDB from './db/db.connection.js';
-import authRoute from './routes/auth.route.js';
-import errorMiddleware from './middlewares/error.middleware.js';
+import connectDB from './database/dbconnecion.js';
+// import authRoute from './routes/auth.route.js';
+// import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
 
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://nimbusbook.vercel.app"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }
@@ -19,9 +19,9 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // API for Authentication
-app.use("/api/v1/auth", authRoute);
+// app.use("/api/v1/auth", authRoute);
 
-app.use(errorMiddleware)
+// app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
 connectDB()
