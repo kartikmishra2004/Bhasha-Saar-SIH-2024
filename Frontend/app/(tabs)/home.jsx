b7c52f29903755ad3b22d186ff2802d7aa4cafa8
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-11-25 01:15:36 +0530

    Refactor

--- Frontend/app/(tabs)/home.jsx
+++ Frontend/app/(tabs)/home.jsx
@@ -2,17 +2,16 @@ import React, { useState } from 'react';
 import {
   View,
   Text,
-  TouchableOpacity,
   TextInput,
   StyleSheet,
   SafeAreaView,
   ScrollView,
   StatusBar,
 } from 'react-native';
-import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 import { Picker } from '@react-native-picker/picker';
 import * as Clipboard from 'expo-clipboard';
-import CustomButton from '../../components/customButton'
+import CustomButton from '../../components/customButton';
 
 export default function App() {
 
@@ -34,7 +33,7 @@ export default function App() {
     mr: "Marathi",
   };
 
-  const [translatedText, setTranslatedText] = useState('Your translation will appear here!!');
+  const [translatedText, setTranslatedText] = useState('');
 
   const handleSubmit = async () => {
     const formData = new FormData();
@@ -64,51 +63,46 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={{ height: '100vh', paddingVertical: 30 }}>
-        <View style={styles.languageSelector}>
-          <Picker
-            onValueChange={(e) => setTransForm({ ...transForm, src_lang: e })}
-            dropdownIconColor="white"
+        <View>
+          <Text
             style={{
-              color: 'white',
-              width: 150,
-              height: 50,
-              padding: 30,
-              backgroundColor: '#232533',
-              borderColor: '#232533',
+              paddingHorizontal: 20,
+              alignItems: 'center',
+              justifyContent: 'center',
+              fontFamily: 'Inder',
+              fontSize: 44,
+              paddingVertical: 25,
+              color: '#ffa001',
+              fontWeight: '900',
             }}>
-            <Picker.Item key={'en'} label='English' value='en' />
-            <Picker.Item key={'hi'} label='Hindi' value='hi' />
-            <Picker.Item key={'bn'} label='Bengali' value='bn' />
-            <Picker.Item key={'gu'} label='Gujrati' value='gu' />
-            <Picker.Item key={'kn'} label='Kannada' value='kn' />
-            <Picker.Item key={'mr'} label='Marathi' value='mr' />
-          </Picker>
-          <Text style={{ color: 'white', padding: 10 }}>To</Text>
-          <Picker
-            onValueChange={(e) => setTransForm({ ...transForm, dest_lang: e })}
-            dropdownIconColor="white"
-            style={{
-              color: 'white',
-              width: 150,
-              height: 50,
-              padding: 30,
-              backgroundColor: '#232533',
-              borderColor: '#232533',
-            }}>
-            <Picker.Item key={'hi'} label='Hindi' value='hi' />
-            <Picker.Item key={'en'} label='English' value='en' />
-            <Picker.Item key={'bn'} label='Bengali' value='bn' />
-            <Picker.Item key={'gu'} label='Gujrati' value='gu' />
-            <Picker.Item key={'kn'} label='Kannada' value='kn' />
-            <Picker.Item key={'mr'} label='Marathi' value='mr' />
-          </Picker>
+            India <Text style={{ color: 'white' }}>Speaks, You</Text> <Text style={{ color: '#ffa001' }}>Understand.</Text>
+          </Text>
         </View>
         <View style={styles.translationBox}>
           <View style={styles.boxHeader}>
-            <Text style={styles.boxTitle}>{languageMapping[transForm.src_lang]}</Text>
+            <Picker
+              mode='dropdown'
+              onValueChange={(e) => setTransForm({ ...transForm, src_lang: e })}
+              dropdownIconColor="white"
+              style={{
+                color: 'white',
+                width: 150,
+                height: 50,
+                padding: 30,
+                backgroundColor: '#232533',
+              }}>
+              <Picker.Item key={'en'} label='English' value='en' />
+              <Picker.Item key={'hi'} label='Hindi' value='hi' />
+              <Picker.Item key={'bn'} label='Bengali' value='bn' />
+              <Picker.Item key={'gu'} label='Gujrati' value='gu' />
+              <Picker.Item key={'kn'} label='Kannada' value='kn' />
+              <Picker.Item key={'mr'} label='Marathi' value='mr' />
+            </Picker>
             <Ionicons name="volume-high" size={20} color="white" />
           </View>
           <TextInput
+            placeholderTextColor="#bababa"
+            placeholder='Enter text to translate.'
             multiline={true}
             scrollEnabled={true}
             onChangeText={(e) => setTransForm({ ...transForm, sentence: e })}
@@ -121,10 +115,29 @@ export default function App() {
         </View>
         <View style={styles.translationBox}>
           <View style={styles.boxHeader}>
-            <Text style={styles.boxTitle}>{languageMapping[transForm.dest_lang]}</Text>
+            <Picker
+              mode='dropdown'
+              onValueChange={(e) => setTransForm({ ...transForm, dest_lang: e })}
+              dropdownIconColor="white"
+              style={{
+                color: 'white',
+                width: 150,
+                height: 50,
+                padding: 30,
+                backgroundColor: '#232533',
+              }}>
+              <Picker.Item key={'hi'} label='Hindi' value='hi' />
+              <Picker.Item key={'en'} label='English' value='en' />
+              <Picker.Item key={'bn'} label='Bengali' value='bn' />
+              <Picker.Item key={'gu'} label='Gujrati' value='gu' />
+              <Picker.Item key={'kn'} label='Kannada' value='kn' />
+              <Picker.Item key={'mr'} label='Marathi' value='mr' />
+            </Picker>
             <Ionicons name="volume-high" size={20} color="white" />
           </View>
           <TextInput
+            placeholderTextColor="#bababa"
+            placeholder='Translated text will appear here.'
             multiline={true}
             scrollEnabled={true}
             style={styles.textInput}
@@ -135,9 +148,10 @@ export default function App() {
           </View>
         </View>
         <View style={{
-          alignItems: 'center'
+          alignItems: 'center',
+          paddingVertical: 20
         }}>
-          {isLoading ? (<CustomButton isDisabled={true} title='Please wait...' />) : (<CustomButton isDisabled={false} handlePress={() => handleSubmit()} style={styles.translateButtonText} title={'Translate'}>
+          {isLoading ? (<CustomButton isDisabled={true} title='Please wait...' />) : (<CustomButton isDisabled={!transForm.sentence ? true : false} handlePress={() => handleSubmit()} style={styles.translateButtonText} title={'Translate'}>
             <Text>Translate</Text>
           </CustomButton>)}
         </View>
@@ -188,17 +202,19 @@ const styles = StyleSheet.create({
   boxTitle: {
     fontSize: 16,
     fontWeight: '600',
-    color: 'white'
+    color: 'white',
+    fontFamily: 'Inder'
   },
   textInput: {
     fontSize: 16,
     backgroundColor: '#2c2e40',
     borderRadius: 10,
-    color: '#bababa',
+    color: 'white',
     padding: 10,
     marginBottom: 10,
     height: 120,
-    textAlignVertical: 'top'
+    textAlignVertical: 'top',
+    fontFamily: 'Inder'
   },
   bottomRow: {
     flexDirection: 'row',
